cdf1487543c118d2ab1dac4424429a71278a315f
Santy <santy@cierco.es>
2022-05-29 19:28:02 +0200

    actualización nombre

--- secure_totp/totp.py
+++ secure_totp/totp.py
@@ -5,7 +5,7 @@ print("mi secreto:", secret)
 
 totp_object = pyotp.TOTP(secret)
 qr_text = totp_object.provisioning_uri(name="mi_usuario", 
-issuer_name="Mi App")
+issuer_name="App TOTP Python")
 print(qr_text)
 
 otp=input("ingresa el otp: ")
