eaffd7165ccd4954f215495e792d9e7d68e1fe0b
Santy <santy@cierco.es>
2022-05-29 19:28:41 +0200

    ok

--- secure_totp/totp.py
+++ secure_totp/totp.py
@@ -4,8 +4,7 @@ secret = pyotp.random_base32()
 print("mi secreto:", secret)
 
 totp_object = pyotp.TOTP(secret)
-qr_text = totp_object.provisioning_uri(name="mi_usuario", 
-issuer_name="App TOTP Python")
+qr_text = totp_object.provisioning_uri(name="mi_usuario", issuer_name="App TOTP Python")
 print(qr_text)
 
 otp=input("ingresa el otp: ")
