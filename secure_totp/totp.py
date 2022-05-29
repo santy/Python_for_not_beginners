4285d96514d40647508b0e0f837714f4bed393d0
Santy <santy@cierco.es>
2022-05-29 19:43:33 +0200

    Actualizado a número secreto fijo

--- secure_totp/totp.py
+++ secure_totp/totp.py
@@ -1,10 +1,12 @@
 import pyotp
 
-secret = pyotp.random_base32()
+#secret = pyotp.random_base32()
+secret = "QWRSBXXD2CWCHHDKKAXVIO3KFJ5WIHZV"
 print("mi secreto:", secret)
 
 totp_object = pyotp.TOTP(secret)
-qr_text = totp_object.provisioning_uri(name="mi_usuario", issuer_name="App TOTP Python")
+qr_text = totp_object.provisioning_uri(name="mi_usuario", 
+issuer_name="Mi App")
 print(qr_text)
 
 otp=input("ingresa el otp: ")
