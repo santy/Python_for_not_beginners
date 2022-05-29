3b8d02e6d26cfeb8723a285c3d32de2de398d551
Santy <santy@cierco.es>
2022-05-29 18:34:48 +0200

    primer programa

---
+++ secure_totp/totp.py
@@ -0,0 +1,24 @@
+import pyotp
+
+secret = pyotp.random_base32()
+print("mi secreto:", secret)
+
+totp_object = pyotp.TOTP(secret)
+qr_text = totp_object.provisioning_uri(name="mi_usuario", 
+issuer_name="Mi App")
+print(qr_text)
+
+otp=input("ingresa el otp: ")
+valid = totp_object.verify(otp)
+print(valid)
+
+
+
+
+
+
+
+
+
+
+
